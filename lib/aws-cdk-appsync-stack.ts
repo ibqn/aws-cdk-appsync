79ee5b284a36f6d68f5710260c984997f9e47806
Evgeny Bobkin <evgen.ibqn@gmail.com>
2021-12-14 00:25:55 +0100

    add user pool

--- lib/aws-cdk-appsync-stack.ts
+++ lib/aws-cdk-appsync-stack.ts
@@ -2,10 +2,36 @@ import * as cdk from '@aws-cdk/core'
 import * as appsync from '@aws-cdk/aws-appsync'
 import * as ddb from '@aws-cdk/aws-dynamodb'
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as cognito from '@aws-cdk/aws-cognito'
 
 export class AwsCdkAppsyncStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
+
+    const userPool = new cognito.UserPool(this, 'cdk-notes-user-pool', {
+      selfSignUpEnabled: true,
+      accountRecovery: cognito.AccountRecovery.PHONE_AND_EMAIL,
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+    })
+
+    const userPoolClient = new cognito.UserPoolClient(
+      this,
+      'cdk-user-pool-client',
+      {
+        userPool,
+      }
+    )
 
     // Creates the AppSync API
     const api = new appsync.GraphqlApi(this, 'Api', {
